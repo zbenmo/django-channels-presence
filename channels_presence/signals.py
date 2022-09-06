e0e6d1043f647c4aee329cca03b6ed1eaa7b6cdf
Oren <zbenmo@gmail.com>
2022-09-06 19:47:07 +0200

    make it work with Django 4.0

--- channels_presence/signals.py
+++ channels_presence/signals.py
@@ -1,5 +1,5 @@
 from django import dispatch
 
 presence_changed = dispatch.Signal(
-    providing_args=["room", "added", "removed", "bulk_change"]
+    # providing_args=["room", "added", "removed", "bulk_change"]
 )
